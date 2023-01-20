2c0a40a15b7812176a880cc8d7d717f25f07f58a
medvedev <medvedev@smarttender.biz>
2023-01-20 14:56:44 +0200

    https://github.com/martinascharrer/vuensight/issues/30#issue-1224190051

--- packages/app/server/index.ts
+++ packages/app/server/index.ts
@@ -4,8 +4,9 @@ import { join } from 'path';
 
 import { parse } from '@vuensight/parser';
 
-export default async (directory: string, webpackConfigPath?: string, tsConfigPath?: string) => {
+export default async (directory: string, webpackConfigPath?: string, tsConfigPath?: string, port?: string) => {
   const app = express();
+  const localPort = port || 4444;
 
   app.get('/parse-result', async (request, response) => {
     const parseResult = await parse(directory, 'vue', webpackConfigPath, tsConfigPath);
@@ -16,8 +17,8 @@ export default async (directory: string, webpackConfigPath?: string, tsConfigPat
 
   app.use(express.static(join(__dirname, '../app')));
 
-  app.listen(4444, () => {
-    console.log('👀 vuensight: http://localhost:4444');
-    console.log('server is listening on port 4444');
+  app.listen(+localPort, () => {
+    console.log(`👀 vuensight: http://localhost:${localPort}`);
+    console.log(`server is listening on port ${localPort}`);
   });
 };
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -6,17 +6,19 @@ import startServer from '@vuensight/app';
 program
     .description('Vue Component Insight CLI')
     .option('-d, --dir [dir]', 'specify the directory that should be analyzed', 'src')
+    .option('-p, --port [port]', 'start the application in a different port (default is 4444)', 'src')
     .option('-wpc, --webpack-config [webpackConfig]', 'path to webpack config file')
     .option('-tsc, --ts-config [tsConfig]', 'path to TypeScript config file')
     .parse();
 
 const dir = program.opts().dir;
+const port = program.opts().port;
 const webpackConfig = program.opts().webpackConfig;
 const tsConfig = program.opts().tsConfig;
 
 const init = async () => {
   try {
-    await startServer(dir, webpackConfig, tsConfig);
+    await startServer(dir, webpackConfig, tsConfig, port);
   } catch (e) {
     console.error('Something went wrong parsing the project', e);
   }
